9169c80f1c7a3ba0b824b7fd5b21b13d45f6f2eb
t03ya <kakstado@gmail.com>
2022-11-07 20:45:25 +0500

    add project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (<header className='header'>
+        <img src='https://i.pinimg.com/originals/be/83/8e/be838ef2e14da0d21ced7bc35e53ade6.png' />
+    </header>
+    )
+}
+
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+    return (<div className='content'>
+        <div> <img src='https://www.euromag.ru/storage/c/2017/02/07/1486458009_997571_58.jpg' width={500} /></div>
+        <div>ava + description</div>
+        <div>My posts
+            <div>New post</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Profile;
